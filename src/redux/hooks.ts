990d80a76d995d82062bfabc12384a25e7e17f8f
ShinCoder <shincoder.forwork@gmail.com>
2025-02-10 00:14:59 +0700

    fix: errors in building mode + replace path alias

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,6 +1,6 @@
-import { useSelector, useDispatch, TypedUseSelectorHook } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { AppDispatch, RootState } from './store';
 
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
