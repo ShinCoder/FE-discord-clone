0a040286fccff6747dc82037d620c8e0dbbc7d36
ShinCoder <shincoder.forwork@gmail.com>
2025-03-26 20:14:45 +0700

    feat: blocked indicator in pinned dm + block send message to blocked user

--- src/components/GlobalModal/UserSettingsModal/components/ProfileTab/components/ColorPicker/index.tsx
+++ src/components/GlobalModal/UserSettingsModal/components/ProfileTab/components/ColorPicker/index.tsx
@@ -1,5 +1,5 @@
-import { Box } from '@mui/material';
-import { MouseEvent } from 'react';
+import { Box, Popover } from '@mui/material';
+import { MouseEvent, useState } from 'react';
 
 interface ColorPickerProps {
   value: string;
@@ -9,6 +9,8 @@ interface ColorPickerProps {
 const ColorPicker = (props: ColorPickerProps) => {
   const { value, onChange } = props;
 
+  const [coord, setCoord] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
+
   return (
     <>
       <Box
@@ -17,10 +19,12 @@ const ColorPicker = (props: ColorPickerProps) => {
           width: '69px',
           height: '50px',
           borderRadius: '4px',
-          backgroundColor: value
+          backgroundColor: value,
+          cursor: 'pointer'
         }}
         onClick={(event: MouseEvent<HTMLDivElement>) => {
           console.log(event.clientX, event.clientY);
+          setCoord({ x: event.clientX, y: event.clientY });
         }}
       ></Box>
     </>
